9a7b6f214c34b8e25178966abf9aecd74a7fa3bd
darkirby <darkirby.tuta.com>
2025-03-16 11:19:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bundle DSFR
+
+Bundle contenant des ressources utiles pour un projet DSFR.
+
+## TODO
---
+++ src/DarkirbyDsfrBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Darkirby\DsfrBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class DarkirbyDsfrBundle extends AbstractBundle
+{
+
+}
