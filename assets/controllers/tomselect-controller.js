9da0ef5bcd53e82d3d9b1630260169028615ea88
darkirby <darkirby.tuta.com>
2025-04-22 15:00:30 +0200

    Tomselect : ajout des options et écriture de la doc

--- assets/controllers/tomselect-controller.js
+++ assets/controllers/tomselect-controller.js
@@ -3,21 +3,40 @@ import { Controller } from '@hotwired/stimulus';
 import TomSelect from 'tom-select';
 
 export default class TomSelectController extends Controller {
+  static values = {
+    options: Object,
+  };
+
   connect() {
-    new TomSelect(this.element, {
-      maxOptions: 10,
-      maxItems: 1,
-      // plugins: ['remove_button'],
-      plugins: {
-        no_backspace_delete: {},
-        clear_button: {
-          title: 'Supprimer la sélection',
-        },
+    // Récupération des options avec valeurs par défaut
+    const number = this.optionsValue['number'] ?? 10;
+    const multiple = this.optionsValue['multiple'] ?? false;
+    const create = this.optionsValue['create'] ?? false;
+
+    // Configuration des plugins en fonction des options
+    const plugins = {
+      no_backspace_delete: {},
+      clear_button: {
+        title: 'Supprimer toute la sélection',
       },
+    };
+    if (multiple) {
+      plugins.remove_button = {
+        title: 'Supprimer cette option',
+      };
+    }
+
+    // On crée le tomselect
+    new TomSelect(this.element, {
+      maxOptions: number,
+      maxItems: multiple ? null : 1,
+      plugins: plugins,
       closeAfterSelect: true,
-      create: false,
+      create: create,
       openOnFocus: false,
       hidePlaceholder: false,
+
+      // On modifie quelques classes CSS à l'initialisaion
       onInitialize: function () {
         this.wrapper.addEventListener('click', () => {
           this.focus();
@@ -25,15 +44,22 @@ export default class TomSelectController extends Controller {
         this.wrapper.classList.add('tomselect');
         this.dropdown.classList.add('fr-card', 'fr-card--shadow');
       },
+
+      // On modifie le rendu pour coller au DSFR
       render: {
         item: function (data, escape) {
-          // return (
-          //   '<button class="fr-tag" aria-label="Retirer la personne" onclick="event.preventDefault();">' +
-          //   escape(data.text) +
-          //   '</button>'
-          // );
-          // return '<p class="fr-tag">'+escape(data.text)+'</p>';
-          return '<span>' + escape(data.text) + '</span>';
+          if (multiple) {
+            return '<span class="fr-tag">' + escape(data.text) + '</span>';
+          } else {
+            return '<span>' + escape(data.text) + '</span>';
+          }
+        },
+        option_create: function (data, escape) {
+          return (
+            '<div class="create option"><span class="fr-icon--sm fr-icon-add-circle-fill fr-mr-1v" aria-hidden="true"></span>Créer l\'option <strong>' +
+            escape(data.input) +
+            '</strong></div>'
+          );
         },
         no_results: function () {
           return '<div class="option">Aucun résultat</div>';
@@ -41,6 +67,7 @@ export default class TomSelectController extends Controller {
       },
     });
 
+    // On cache le select initial (la valeur sera répliquée dessus automatiquement)
     this.element.classList.add('fr-hidden');
   }
 }
--- docs/tomselect.md
+++ docs/tomselect.md
@@ -31,3 +31,38 @@ Pour intégrer le css et le controller stimulus au projet, ajouter les imports s
     ```
 
 ## Utilisation
+
+Dans un FormType, ajouter les attributs suivants dans le `attr` des options d'un champ :
+
+```php
+'data-controller' => 'tomselect',
+'data-tomselect-options-value' => json_encode([
+    'create' => true,
+    ...
+]),
+```
+
+L'attribut `data-tomselect-options-value` peut-être omis pour utiliser toutes les options par défaut. Sinon, il peut contenir les clés suivantes :
+
+- **number**: *int* (optionnel, défaut 10) Nombres d'options à afficher dans le menu déroulant
+- **multiple**: *bool* (optionnel, défaut false) Champ à sélection multiple ou non
+- **create**: *bool* (optionnel, défaut false) Autorise l'utilisateur à créer ces propres valeurs
+
+Exemple :
+
+```php
+ ->add('personne', EntityType::class, [
+        'required' => true,
+        'class' => Personne::class,
+        'label' => 'Choisir une personne',    
+        'attr' => [
+            'data-controller' => 'tomselect',
+            'data-tomselect-options-value' => json_encode([
+                'number' => 20,
+                'multiple' => true,
+                'create' => false,
+            ]),
+        ]
+        'placeholder' => 'Taper les premières lettres du nom/prénom',
+    ])
+```
