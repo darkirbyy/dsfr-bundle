9c7c066817cb0fce57bad4d138665963cf39cc70
darkirby <darkirby.tuta.com>
2025-04-22 13:04:57 +0200

    Tomselect : simplification et stylisation en mode single

--- assets/controllers/tomselect-controller.js
+++ assets/controllers/tomselect-controller.js
@@ -7,37 +7,32 @@ export default class TomSelectController extends Controller {
     new TomSelect(this.element, {
       maxOptions: 10,
       maxItems: 1,
-      plugins: ['remove_button'],
+      // plugins: ['remove_button'],
       // plugins: ['clear_button'],
       closeAfterSelect: true,
       create: false,
       openOnFocus: false,
+      hidePlaceholder: false,
       onInitialize: function () {
-        // Supprime les classes de Tom Select
-        this.wrapper.classList.remove('fr-select');
         this.wrapper.classList.add('tomselect');
         this.dropdown.classList.add('fr-card', 'fr-card--shadow');
-        this.wrapper.querySelector('input').classList.add('fr-input');
       },
-      // onItemAdd: function(value, $item){
-      //     console.log(this.wrapper.querySelector('input'));
-      //     this.wrapper.querySelector('input').value = value;
-      // },
       render: {
         item: function (data, escape) {
-          return (
-            '<button class="fr-tag fr-mr-1v" aria-label="Retirer la personne" onclick="event.preventDefault();">' +
-            escape(data.text) +
-            '</button>'
-          );
-          // return '<p class="fr-tag fr-mr-2v">'+escape(data.text)+'</p>';
-          // return '<span>'+escape(data.text)+'</span>';
+          // return (
+          //   '<button class="fr-tag" aria-label="Retirer la personne" onclick="event.preventDefault();">' +
+          //   escape(data.text) +
+          //   '</button>'
+          // );
+          // return '<p class="fr-tag">'+escape(data.text)+'</p>';
+          return '<span>' + escape(data.text) + '</span>';
         },
         no_results: function () {
           return '<div class="option">Aucun résultat</div>';
         },
       },
     });
+
     this.element.classList.add('fr-hidden');
   }
 }
